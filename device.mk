6000d7279a9f983a0f4d57023e290ed5975723c5
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-11-04 23:25:53 -0400

    sake: Build an additional radio lib
    * We need this for radio to actually initialize since some parts of
      IMS still depends on this most probably since the blobs are from
      Android 11.
    
    Change-Id: Iab4f628f681092cfeedc1047a0fec88e569700b4

--- device.mk
+++ device.mk
@@ -317,6 +317,7 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.secure_element@1.2.vendor \
